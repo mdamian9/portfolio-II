4031b8ea3ce7d744a1876a1bd477c915e23ff636
mdamian9 <m.damian9@hotmail.com>
2019-08-09 01:20:36 -0700

    Installed more dependencies, added scripts to package.json - tested server with postman

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
-const indexRoutes = require('./api/routes/');
+const msgRoutes = require('./api/routes/messages');
 
-app.use('/', indexRoutes);
+app.use('/message', msgRoutes);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+};
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
