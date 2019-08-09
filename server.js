1f2712d556b69b93bf576cfaeff7e0f8e6e26b61
mdamian9 <m.damian9@hotmail.com>
2019-08-08 23:17:55 -0700

    Added express, created server & api/routes - express server is runnning

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const indexRoutes = require('./api/routes/');
+
+app.use('/', indexRoutes);
+
+app.listen(PORT, (req, res, next) => {
+    console.log(`Listening on PORT ${PORT}`);
+});
