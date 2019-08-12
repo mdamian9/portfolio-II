6b18101a94b24a3c68b468f5cce15e17ed5308ef
mdamian9 <m.damian9@hotmail.com>
2019-08-12 12:24:44 -0700

    Modified app.js / created HomePage component, added images / files to use

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+
+const HomePage = () => {
+    return (
+        <Container>
+            <Col>
+                <Row>
+                    Home Page Component
+            </Row>
+            </Col>
+        </Container>
+    );
+};
+
+export default HomePage;
